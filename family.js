b61529002acee4b235fdace085fc7fd10d81ae43
Ranjini <ranjini2410@gmail.com>
2026-04-28 12:15:46 +0530

    updated all

--- family.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("info").innerText =
-"Good Daughter.";
---
+++ naan/naan.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const toast = document.getElementById("toast");
+
+  // Set message
+  toast.innerText = "Welcome to Daughter's Page 👋";
+
+  // Show toast (slide down)
+  setTimeout(() => {
+    toast.style.top = "20px";
+  }, 100);
+
+  // Hide toast after 3 seconds
+  setTimeout(() => {
+    toast.style.top = "-100px";
+  }, 3000);
+
+});
